cfbb691fa3e0770dc1e76251bccedfcf2b80fc9c
Badsha114 <98599980+Badsha114@users.noreply.github.com>
2022-02-20 15:42:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Tellybots/Uploader-Bot /Uploader-Bot
+git clone https://github.com/Badsha114/Uploader-Bot /Uploader-Bot
 cd /Uploader-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
